6aa433fd3ed5030e4170e22778fdad846de42374
ethanc8 <echaroenpitaks@gmail.com>
2025-07-18 11:53:33 -0500

    Add BNSF Line scenarios

---
+++ scenarios/metra/bnsf/highlevel/nonpeak_local.py
@@ -0,0 +1,100 @@
+scenario_name = "BNSF Line - current non-peak locals, high-level boarding"
+
+# convert mph to km/h
+def mph(speed):
+    return speed * 1.609344
+
+# Each tuple: (start milepost, end milepost, speed limit (km/h), ends at stop, name)
+
+# Mileposts are from https://chicagorailfan.com/mmbnsf.html
+# Speed limits are from OpenRailwayMap
+
+route = [
+                                # CHICAGO UNION STATION stop
+    (  0.0,   0.6, mph(15), False),
+    (  0.6,   0.8, mph(30), False),
+    (  0.8,   1.4, mph(25), False),
+    (  1.4,   1.8, mph(35), True),
+                                # HALSTED ST stop
+    (  1.8,   2.3, mph(40), False),
+    (  2.3,   3.7, mph(60), True),
+                                # WESTERN AVE stop
+    (  3.7,   6.9, mph(70), True),
+                                # CICERO stop
+    (  6.9,   9.0, mph(70), False),
+                                # skip LA VERGNE
+    (  9.0,   9.5, mph(70), True),
+                                # BERWYN stop
+    (  9.5,  10.1, mph(70), True),
+                                # HARLEM AVE stop
+    ( 10.1,  11.0, mph(70), True),
+                                # RIVERSIDE stop
+    ( 11.0,  11.8, mph(70), True),
+                                # HOLLYWOOD stop
+    ( 11.8,  12.4, mph(70), True),
+                                # BROOKFIELD stop
+    ( 12.4,  13.0, mph(70), True),
+                                # skip CONGRESS PARK
+    ( 13.0,  13.7, mph(70), True),
+                                # LA GRANGE RD stop
+    ( 13.7,  14.0, mph(70), True),
+                                # skip STONE AVE
+    ( 14.0,  15.4, mph(70), True),
+                                # WESTERN SPRINGS stop
+    ( 15.4,  16.3, mph(70), False),
+                                # skip HIGHLANDS
+    ( 16.3,  16.8, mph(70), True),
+                                # HINSDALE stop
+    ( 16.8,  17.7, mph(70), False),
+                                # skip WEST HINSDALE
+    ( 17.7,  18.2, mph(70), True),
+                                # CLARENDON HILLS stop
+    ( 18.2,  19.5, mph(70), True),
+                                # WESTMONT stop
+    ( 19.5,  20.3, mph(70), True),
+                                # FAIRVIEW AVE stop
+    ( 20.3,  21.1, mph(70), True),
+                                # DOWNERS GROVE stop
+    ( 21.1,  22.8, mph(70), True),
+                                # BELMONT stop
+    ( 22.8,  24.4, mph(70), True),
+                                # LISLE stop
+    ( 24.4,  28.4, mph(70), True),
+                                # NAPERVILLE stop
+    ( 28.4,  31.7, mph(70), True),
+                                # ROUTE 59 stop
+    ( 31.7,  35.4, mph(70), False),
+    ( 35.4,  37.4, mph(55), True),
+                                # AURORA stop
+]
+
+stops = {
+    0.0: 'Chicago Union Station',
+    1.8: 'Halsted St',
+    3.7: 'Western Ave',
+    6.9: 'Cicero',
+    9.0: 'LaVergne',
+    9.5: 'Berwyn',
+    10.1: 'Harlem Ave',
+    11.0: 'Riverside',
+    11.8: 'Hollywood',
+    12.4: 'Brookfield',
+    13.0: 'Congress Park',
+    13.7: 'LaGrange Rd',
+    14.0: 'Stone Ave',
+    15.4: 'Western Springs',
+    16.3: 'Highlands',
+    16.8: 'Hinsdale',
+    17.7: 'West Hinsdale',
+    18.2: 'Clarendon Hills',
+    19.5: 'Westmont',
+    20.3: 'Fairview Ave.',
+    21.1: 'Downers Grove',
+    22.8: 'Belmont',
+    24.4: 'Lisle',
+    28.4: 'Naperville',
+    31.7: 'Route 59',
+    37.4: 'Aurora'
+}
+
+dwell_time = 30 # seconds
